2cdf705a93bde1c150d6047bb22cc229edc2afa8
vishalk-17 <vk0262401@gmail.com>
2025-04-27 11:26:55 +0530

    add message models

--- backend/models/message.js
+++ backend/models/message.js
@@ -3,5 +3,6 @@ const messageSchema = new Schema({
   user: String,
   message: String,
   timestamp: { type: Date, default: Date.now }
+  //add
 });
 module.exports = model('message' , messageSchema);
