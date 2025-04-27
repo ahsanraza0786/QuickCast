fe0d7fd46bcfceb7973a186cd7870b8d16650c59
vishalk-17 <vk0262401@gmail.com>
2025-04-27 11:16:09 +0530

    to add pollcreater

--- frontend/src/components/PollCreater.jsx
+++ frontend/src/components/PollCreater.jsx
@@ -101,7 +101,7 @@ export default function PollCreator({ roomCode, socket }) {
           <button
             onClick={addOption}
             type="button"
-            className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            className="w-full flex justify-center py-2 px-4 border border-gray-301 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
             + Add Option
           </button>
