0a2bd2390087b8e24638085dc2bb7b3068c9df0d
anis5165 <anishkumargvm02@gmail.com>
2025-05-13 23:58:25 +0530

    chore: clean up whitespace in LandingPage component

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,10 +1,5 @@
 "use client";
-// import React, { useEffect } from 'react';
-// import Link from 'next/link';
-// import { Clock, Users, BarChart2 } from 'lucide-react';
-// import Navbar from '@/components/Navbar';
-// import 'aos/dist/aos.css'; // Import AOS styles
-// import AOS from 'aos'; // Import AOS library
+
 
 import Home from "../components/Home";
 
@@ -13,6 +8,9 @@ import Home from "../components/Home";
 const LandingPage = () => {
 
   return <Home/>
+
+
+
   
 };
 
